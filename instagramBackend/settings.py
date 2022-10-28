e0abca23f157af090b94e31cfe59fc80f45f349e
RubenStark <rubenlop_navarrete@hotmail.com>
2022-10-27 21:02:33 -0500

    debug true

--- instagramBackend/settings.py
+++ instagramBackend/settings.py
@@ -205,6 +205,3 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = 'instagrambackendclone'
 
-
-if os.getcwd() == '/app':
-    DEBUG = False
