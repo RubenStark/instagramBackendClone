ba6f5d313b36b4149be89f39fe77ce0315cb5eca
RubenStark <rubenlop_navarrete@hotmail.com>
2022-11-03 12:17:18 -0600

    email admin

--- instagram/admin.py
+++ instagram/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Post, Comment, Story, Notification
+from .models import Profile, Post, Comment, Story, Notification, Email
 # Register your models here.
 
 admin.site.register(Profile)
@@ -7,3 +7,4 @@ admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Story)
 admin.site.register(Notification)
+admin.site.register(Email)
